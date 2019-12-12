14faa6eab89ac8d3ba97fbc26b40f1b2585629d7
leeum <liam.tate@hotmail.com>
2019-12-11 20:49:44 -0500

    added submit button function

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-app.get("/reservations", function (req, res) {
+app.get("/reservation", function (req, res) {
     res.sendFile(path.join(__dirname, "reservation.html"));
 });
 
